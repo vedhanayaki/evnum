20481b681136d0661cc20f8275355ccbbd71c381
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-30 11:35:16 +0530

    Add files via upload

---
+++ evennumberbtw2interval.java
@@ -0,0 +1,15 @@
+package guviset3;
+
+public class evennumberbtw2interval {
+
+	public static void main(String[] args) {
+		int a=100;
+		for(int i=0;i<a;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
